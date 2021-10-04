9ebb47aeb3f260c67af3cd5def6edda7da8f9134
LyonyaZhozhikov <jlr10@mail.ru>
2021-10-05 03:06:32 +0900

    123
    123

--- units_converterLZ.py
+++ units_converterLZ.py
@@ -1,7 +1,7 @@
 # startuem
 
 while True:
-    x = input("soo what do you wanna know? outside temp?? \n please put your data to convert (like this 12F or 99C) : ")
+    x = input("soo what do you wan de temp?? \n please put y nvert (like this 12F or 99C) : ")
     i_convention = x[-1]
     result = ""
     edin_izmer = ""
@@ -10,13 +10,13 @@ while True:
         result = int(round((9 * degree) / 5 + 32))
         edin_izmer = "Fahrenheit"
         print("\n The temperature in", edin_izmer, "is", result, "degrees.")
-        print("please note that you can leave this app by 'exit' command at the next input\n")
+        print("please note that  is app by 'exit' command at the next input\n")
     elif i_convention.upper() == "F":
         degree = int(x[:-1])
         result = int(round((degree - 32) * 5 / 9))
         edin_izmer = "Celsius"
         print("\n The temperature in", edin_izmer, "is", result, "degrees.")
-        print("please note that you can leave this app by 123 123 command at the next input\n")
+        print("please note that ynput\n")
     else:
         print("\n please point your edinici izmereniya properly \n")
 print("gg")
