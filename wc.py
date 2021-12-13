8dc443ec4bb3be9d5edfcbd5f77e15382bbc3892
LyonyaZhozhikov <90510421+LyonyaZhozhikov@users.noreply.github.com>
2021-12-14 04:21:02 +0900

    commit
    wc is not working here

--- wc.py
+++ wc.py
@@ -14,29 +14,34 @@ import argparse
 
 
 def run(args):
-    def count_lines(args):
-        lines = 0
-        for line in args.filename:
-            lines += 1
-        sys.stdout.write(str(lines))
+    def count_lines():
+        if line_s:
+            lines = 0
+            for line in args.filename:
+                lines += 1
+            sys.stdout.write(str(lines))
 
-    def count_words(args):
-        words = 0
-        for line in args.filename:
-            words += len(line.split(" "))
-        sys.stdout.write(str(words))
+    def count_words():
+        if word_s:
+            word = 0
+            for line in args.filename:
+                word += len(line.split(" "))
+            sys.stdout.write(str(word))
 
-    def count_bytes(args):
-        sys.stdout.write(sys.getsizeof(args.filename))
-    whole = str(count_lines(args.filename), count_words(args.filename), count_bytes(args.filename))
-    sys.stdout.write(whole)
+    def count_bytes():
+        if byte_s:
+            sys.stdout.write(str(sys.getsizeof(args.filename)))
+    count_lines()
+    count_words()
+    count_bytes()
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Displays list of files in directory")
+    parser = argparse.ArgumentParser(description="Counting words lines and bytes of file")
     parser.add_argument('filename', help="input text file", nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('-l', help="Showing lines", dest="lines", action='store_false')
-    parser.add_argument('-w', help="Showing words", dest="words", action='store_false')
-    parser.add_argument('-c', help="Showing bytes", dest="bytes", action='store_false')
+    parser.add_argument('-l', help="Showing lines", dest="line_s", action='store_true')
+    parser.add_argument('-w', help="Showing words", dest="word_s", action='store_true')
+    parser.add_argument('-c', help="Showing bytes", dest="byte_s", action='store_true')
     parser.set_defaults(func=run)
     args = parser.parse_args()
     args.func(args)
