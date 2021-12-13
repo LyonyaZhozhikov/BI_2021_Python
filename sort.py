27e51333f128a0909facf44391e30b3a840241ab
LyonyaZhozhikov <90510421+LyonyaZhozhikov@users.noreply.github.com>
2021-12-14 00:06:36 +0900

    Added precreated scripts from RESURRECTION branch
    ls
    sort

---
+++ sort.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# start
+# 3. sort - sort words in file | -
+
+#
+
+import sys
+import argparse
+
+
+def run(args):
+    lists = args.filename.readlines()
+    pre_sort_ed = []
+    for line in lists:
+        stripped_line = line.strip()
+        line_list = stripped_line.split()
+        pre_sort_ed.append(line_list)
+    sort_ed = sorted(pre_sort_ed)
+    for i in sort_ed:
+        sys.stdout.write('  '.join(i))
+        sys.stdout.write('\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Displays sorted lines in file")
+    parser.add_argument('filename', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
